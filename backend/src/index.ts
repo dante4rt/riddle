238b93e681e50e6af2d65b1054c0c82867f3b465
Dante4rt <rxmxdhxni@gmail.com>
2025-04-16 18:23:56 +0700

    refactor: reorder imports in index.ts for better organization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
 import compression from "compression";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
 import mongoose from "mongoose";
 
 import authRoute from "./routes/auth";
