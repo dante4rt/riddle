8f1505f440f0d9b63701f11b517ffe6f8e07409b
Dante4rt <rxmxdhxni@gmail.com>
2025-04-25 23:20:10 +0700

    feat: add Monad Testnet

--- backend/src/constants/config.ts
+++ backend/src/constants/config.ts
@@ -1,11 +1,13 @@
 export const CONTRACT_ADDRESSES = {
     11155111: "0x8C4c828c6C88352F02E3D5649F8416DAC45dBBb0",
     84532: "0x471FE11ecDc6efF32687a1237F6E9C6E00dF0411",
-    4202: "0x7b49360b03102fBA5C6b05Aa7217F30965439226"
+    4202: "0x7b49360b03102fBA5C6b05Aa7217F30965439226",
+    10143: "0x6880809cCa8D3774dFD9270e3A946682E2cb029F"
 };
 
 export const RPC_URLs = {
     11155111: "https://api.zan.top/public/eth-sepolia",
     84532: "https://sepolia.base.org",
-    4202: "https://rpc.sepolia-api.lisk.com"
+    4202: "https://rpc.sepolia-api.lisk.com",
+    10143: "https://testnet-rpc.monad.xyz"
 }
--- frontend/app/services/wagmi/config.ts
+++ frontend/app/services/wagmi/config.ts
@@ -6,16 +6,18 @@ import { http } from 'viem';
 import {
     baseSepolia,
     liskSepolia,
+    monadTestnet,
     sepolia,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
     appName: 'Riddle',
     projectId: "PROJECT_ID",
-    chains: [sepolia, baseSepolia, liskSepolia],
+    chains: [sepolia, baseSepolia, liskSepolia, monadTestnet],
     transports: {
         [sepolia.id]: http(),
         [baseSepolia.id]: http(),
         [liskSepolia.id]: http(),
+        [monadTestnet.id]: http(),
     },
 });
