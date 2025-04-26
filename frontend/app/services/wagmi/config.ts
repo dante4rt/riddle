b40898abdc13a579fd8e7d0a736b98184258aa70
Dante4rt <rxmxdhxni@gmail.com>
2025-04-26 11:20:21 +0700

    fix: update wagmi config to include icons for LiskSepolia, Monad, and MegaETH chains

--- frontend/app/services/wagmi/config.ts
+++ frontend/app/services/wagmi/config.ts
@@ -1,20 +1,21 @@
-import {
-    getDefaultConfig,
-} from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-import { http } from 'viem';
-import {
-    baseSepolia,
-    liskSepolia,
-    megaethTestnet,
-    monadTestnet,
-    sepolia,
-} from 'wagmi/chains';
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { http } from "viem";
+import { baseSepolia, liskSepolia, megaethTestnet, monadTestnet, sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-    appName: 'Riddle',
+    appName: "Riddle",
     projectId: "PROJECT_ID",
-    chains: [sepolia, baseSepolia, liskSepolia, monadTestnet, megaethTestnet],
+    chains: [
+        sepolia,
+        baseSepolia,
+        { ...liskSepolia, iconUrl: "/chains/LiskSepolia.svg" },
+        { ...monadTestnet, iconUrl: "/chains/Monad.svg" },
+        {
+            ...megaethTestnet,
+            iconUrl: "/chains/MegaETH.png",
+        },
+    ],
     transports: {
         [sepolia.id]: http(),
         [baseSepolia.id]: http(),
